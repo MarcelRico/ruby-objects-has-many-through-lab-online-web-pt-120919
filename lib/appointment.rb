c6e8fc8290edd8ea8fa371977a63503361426130
Marcel Rico <personal.mr4@gmail.com>
2020-02-18 03:16:56 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -4,7 +4,10 @@ class Appointment
   
   attr_accessor :date,:patient,:doctor
   
-  def initialize()
-    
+  def initialize(date,patient,doctor)
+    @date = date
+    @patient = patient
+    @doctor = doctor
+    @@all << self
   end
 end
